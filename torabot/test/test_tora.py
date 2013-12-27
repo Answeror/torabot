ef6f9ee1c33e105f8e3daa92d783a26950507f8c
answeror <answeror@gmail.com>
2013-12-28 00:00:03 +0800

    tora id from order uri tested

--- torabot/test/test_tora.py
+++ torabot/test/test_tora.py
@@ -1,9 +1,14 @@
 from nose.tools import assert_equal
-from ..tora import order_uri_from_toraid
+from ..tora import order_uri_from_toraid, toraid_from_order_uri
+
+
+URI = 'http://www.toranoana.jp/mailorder/article/04/0030/17/39/040030173983.html'
+ID = '040030173983'
 
 
 def test_order_uri_from_toraid():
-    assert_equal(
-        order_uri_from_toraid('040030173983'),
-        'http://www.toranoana.jp/mailorder/article/04/0030/17/39/040030173983.html'
-    )
+    assert_equal(order_uri_from_toraid(ID), URI)
+
+
+def test_toraid_from_order_uri():
+    assert_equal(toraid_from_order_uri(URI), ID)
--- torabot/tora.py
+++ torabot/tora.py
@@ -1,7 +1,13 @@
 from nose.tools import assert_equal
+import re
 
 
 def order_uri_from_toraid(id):
     assert_equal(len(id), 12)
     base = 'http://www.toranoana.jp/mailorder/article/%s/%s/%s/%s/%s.html'
     return base % (id[:2], id[2:6], id[6:8], id[8:10], id)
+
+
+def toraid_from_order_uri(uri):
+    m = re.search(r'.*/(\d{12})\.html', uri)
+    return None if m is None else m.group(1)
