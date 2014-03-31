1a5da7598e7076449c2058f7ec3f9a5436ee5241
answeror <answeror@gmail.com>
2014-03-31 16:17:43 +0800

    make extension manager global

--- torabot/core/mod.py
+++ torabot/core/mod.py
@@ -1,20 +1,26 @@
 from stevedore.driver import DriverManager
 from stevedore.extension import ExtensionManager
 from .local import get_current_conf
+from ..ut.bunch import Bunch
+
+
+g = Bunch()
+
+
+def manager():
+    m = g.get('manager')
+    if m is None:
+        m = g['manager'] = ExtensionManager(
+            'torabot.mods',
+            invoke_on_load=True,
+            invoke_args=(get_current_conf(),)
+        )
+    return m
 
 
 def mod(name):
-    return DriverManager(
-        'torabot.mods',
-        name,
-        invoke_on_load=True,
-        invoke_args=(get_current_conf(),),
-    ).driver
+    return manager()[name]
 
 
 def mods():
-    return [e.obj for e in ExtensionManager(
-        'torabot.mods',
-        invoke_on_load=True,
-        invoke_args=(get_current_conf(),)
-    )]
+    return [e.obj for e in manager()]
