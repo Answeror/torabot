2085eee9a072bd3e305b894235534767fdde1586
answeror <answeror@gmail.com>
2014-04-11 14:18:31 +0800

    convert bilibili bgmcount to int

--- torabot/mods/bilibili/views/web.py
+++ torabot/mods/bilibili/views/web.py
@@ -42,7 +42,7 @@ def format_sp_notice_body(notice):
     return "bilibili: <a href='%(uri)s'>%(title)s</a> 更新至第%(n)d话" % dict(
         uri=quote('http://www.bilibili.tv/sp/' + notice.change.sp.title),
         title=notice.change.sp.title,
-        n=notice.change.sp.bgmcount,
+        n=int(notice.change.sp.bgmcount),
     )
 
 
