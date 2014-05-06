d7f94174f90d42f9ad70a7215fbf96c7c3f2bee6
answeror <answeror@gmail.com>
2014-05-07 02:24:06 +0800

    only notice activated user

--- torabot/tasks/notice.py
+++ torabot/tasks/notice.py
@@ -1,7 +1,7 @@
 from .engine import make as make_engine
 from ..ut.connection import ccontext
 from ..ut.guard import timeguard
-from ..db import get_pending_notices
+from .. import db
 from ..core.notice import send_notice
 
 
@@ -10,12 +10,13 @@ def notice_all(conf):
     engine = make_engine(conf)
 
     with ccontext(engine=engine) as conn:
-        notices = get_pending_notices(conn)
+        notices = db.get_pending_notices(conn)
 
     for notice in notices:
         with ccontext(commit=True, engine=engine) as conn:
-            send_notice(
-                conf=conf,
-                notice=notice,
-                conn=conn,
-            )
+            if db.user_activated_bi_id(notice.user_id):
+                send_notice(
+                    conf=conf,
+                    notice=notice,
+                    conn=conn,
+                )
