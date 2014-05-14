6f25d9a5ee4ec959f16ebc7fe5fa208ed9d9c40d
answeror <answeror@gmail.com>
2014-05-14 22:41:39 +0800

    add yande.re empty query; update readme to include yande.re

--- torabot/mods/yandere/__init__.py
+++ torabot/mods/yandere/__init__.py
@@ -2,7 +2,6 @@ from ...ut.bunch import bunchr
 from ..base import Mod
 from ..mixins import (
     ViewMixin,
-    NoEmptyQueryMixin,
     make_blueprint_mixin
 )
 from .query import get_query_text
@@ -13,7 +12,6 @@ name = 'yandere'
 
 class Yandere(
     ViewMixin,
-    NoEmptyQueryMixin,
     make_blueprint_mixin(__name__),
     Mod
 ):
@@ -22,6 +20,7 @@ class Yandere(
     has_advanced_search = False
     description = '二次元高清图站, 直接订阅诸如 https://yande.re/post?tags=pantyhose 的链接.'
     normal_search_prompt = '订阅地址/tags'
+    allow_empty_query = True
 
     def view(self, name):
         from .views import web, email
