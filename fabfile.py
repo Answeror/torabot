11baf15d2607710b7e77917e1d1d75ba37b475e5
answeror <answeror@gmail.com>
2014-07-09 11:40:03 +0800

    update gunicorn, use multithreading provided by gunicorn

--- fabfile.py
+++ fabfile.py
@@ -43,4 +43,4 @@ def gunicorn():
                     run('python setup.py develop')
                     runbg('celery worker -A torabot -f data/celery-worker.log --autoscale=4,1')
                     runbg('celery beat -A torabot -f data/celery-beat.log')
-                    runbg('gunicorn --pythonpath . -t 600 -w 2 -k gunicorn_worker.Worker gunicorn_app:app')
+                    runbg('gunicorn --pythonpath . -t 600 -w 2 -k sync --threads 16 gunicorn_app:app')
